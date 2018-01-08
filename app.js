5309066c21784e19c694813b4a2ded7c7429402b
Geomovil <gonzalo.murgueytio@geomovil.net>
2018-01-08 14:34:29 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser')
 
 
 // Inicializar variables
 var app = express();
+
+
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 //conexion a la base de datos
@@ -16,12 +29,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error, res)
 
 
 //Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
-});
+app.use('/login', loginRoutes);
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
+
+
 
 
 // Escuchar peticiones
